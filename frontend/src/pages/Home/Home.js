8ebaf8340177c8cad5d60931e1d3e6382f06a6a4
Alejandro ZZ <ale0720199@gmail.com>
2023-07-06 20:21:13 -0400

    home page's design done

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import "./Home.css";
 
 export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home__background">
+      <div className="container home__container">
+        <h1>Let's record your fitness Today!</h1>
+        <h3>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the
+        </h3>
+        <button className="btn home__btn">Get started</button>
+      </div>
+    </div>
+  );
 }
