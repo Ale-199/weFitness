ee26c3e8263a762bab2ac7e7bf8cfb9c2756dcd4
Alejandro ZZ <ale0720199@gmail.com>
2023-07-07 20:25:57 -0400

    signup page's design done

--- frontend/src/pages/Signup/Signup.js
+++ frontend/src/pages/Signup/Signup.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import "./Signup.css";
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className="bg">
+      <div className="signup__container container">
+        <div className="signup">
+          <h3>Sign Up</h3>
+          <input type="text" placeholder="User Name:" />
+          <input type="text" placeholder="Email:" />
+          <input type="text" placeholder="Password:" />
+          <button className="btn">Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
 }
